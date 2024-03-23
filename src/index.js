fbcce2437e0b8f97c1eafc4a9d69cf40a1060fdd
Rezmii <bartoszrezmer20@gmail.com>
2024-03-23 14:14:50 +0100

    dialog element set up

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import { Set } from "./sets.js";
 import { displaySets } from "./domManipulation.js";
 
 const setArr = [];
+const dialog = document.querySelector("dialog");
+const showDialogButton = document.querySelector("#show-dialog-button");
+const closeButton = document.querySelector("#close-button");
 const addSetButton = document.querySelector("#add-set-button");
-addSetButton.addEventListener("click", () => console.log("XD"));
+
+showDialogButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+addSetButton.addEventListener("click", () => {});
 
 const todoes1 = new Todo("1", "2", "3", "4");
+
 const set1 = new Set(1, "Fitness", "⚽️", "red");
 const set2 = new Set(2, "Study", "📖", "blue");
 set1.addSetToArray(setArr);
 set2.addSetToArray(setArr);
+
 displaySets(setArr);
 console.log(setArr);
